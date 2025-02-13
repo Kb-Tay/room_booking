7c6247ee7e633d2f3d56e96a19f2dee588698db6
Aaron <taykaiboon12@gmail.com>
2025-02-13 15:55:38 +0800

    init basic application

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+});
